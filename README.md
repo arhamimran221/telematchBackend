859c8c09c8921f2024fb4770506575c3fdda6f2e
arhamimran221 <arham.appcorp@gmail.com>
2024-09-03 00:21:18 +0500

    Added workflow

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 telematch-backend
+# telematchBackend
--- config/db.js
+++ config/db.js
@@ -3,14 +3,15 @@ const dotenv = require("dotenv");
 
 dotenv.config();
 
-const pool = mysql.createPool({
+const pool = mysql.createConnection({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
-  waitForConnections: true,
-  connectionLimit: 10,
-  queueLimit: 0,
+  port: process.env.DB_PORT,
+  ssl: {
+    rejectUnauthorized: false, // adjust as necessary
+  },
 });
 
 module.exports = pool.promise();
